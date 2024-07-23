e4f75f6002a43fbf8ac59a727c4d35949467c86e
aaryashende03 <168561498+aaryashende03@users.noreply.github.com>
2024-07-23 17:47:05 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+	
+int main()
+{
+	//Case 1:Addition
+	
+	int x , y;
+	cout<<"Enter the first number ="<<"\n";
+	cin>>x;
+	cout<<"Enter the the second number = "<<"\n";
+	cin>>y;
+	int sum = x + y;
+	cout<<"Addition of nos is :\n"<<sum;
+	
+    //Case 2:Subtraction
+	
+	cout<<"Enter the first number = "<<"\n";
+	cin>>x;
+	cout<<"Enter the the second number ="<<"\n";
+	cin>>y;
+	int sub = x - y;
+	cout<<"Subtraction of nos is:\n"<<sub;
+	
+	//Case 3: Multiplication
+	
+	cout<<"Enter the the first number = "<<"\n";
+	cin>>x;
+	cout<<"Enter the second number ="<<"\n";
+	cin>>y;
+	int mul = x*y;
+	cout<<"Multiplicationof nos is\n"<<mul;
+	
+	//Case 4: Division
+	
+	cout<<"Enter the first number = "<<"\n";
+	cin>>x;
+	cout<<"Enter the second number = "<<"\n";
+	cin>>y;
+	int div = x/y;
+	cout<<"Division of nos is :\n"<<div;
+	
+	return 0;
+	
+}
+
+	
+	
+	
+	
+
+
